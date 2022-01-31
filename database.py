5e80d2f11875f5f4be89a49f5a5e1e3c8b7db0e2
rgorish3 <rgorish3@gmail.com>
2022-01-31 00:07:54 -0500

    Added cursor to connect() in database.py

--- database.py
+++ database.py
@@ -4,28 +4,26 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# try:
 db = mysql.connector.connect(user=os.getenv("DB_USER"), 
     password=os.getenv("DB_PASSWORD"),
     host=os.getenv("DB_HOST"),
     database=os.getenv("DB_DATABASE")
 )
 print('Connected to Database')
-# except mysql.connector.Error as err:
-#     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-#         print("Something is wrong with your user name or password")
-#     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-#         print("Database does not exist")
-#     else:
-#         print(err)
+
+cursor = db.cursor()
+
 
 
 def connect():
+    global db
     db=mysql.connector.connect(user=os.getenv("DB_USER"), 
     password=os.getenv("DB_PASSWORD"),
     host=os.getenv("DB_HOST"),
     database=os.getenv("DB_DATABASE")
     )
-    print('Reonnected to Database')
+    print('Reconnected to Database')
+    
+    global cursor 
+    cursor= db.cursor()
 
-cursor = db.cursor()
